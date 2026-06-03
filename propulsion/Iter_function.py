8c3f016715acb1dd072ebbf50f82cee406699a7c
ERodrigues175 <E.GarciaRodrigues@student.tudelft.nl>
2026-06-03 15:26:11 +0200

    Updated prop to include motor and ESC weight

--- propulsion/Iter_function.py
+++ propulsion/Iter_function.py
@@ -32,7 +32,7 @@ def find_prop(MTOW,N_prop, prop_list):
         check, cond = OEI_performance_check(N_prop, T_req, prop)
         if check:
             P_req = get_power_required(N_prop, T_req, prop)
-            options[name]= {"OEI_condition": cond, "Power_required": P_req}
+            options[name]= {"OEI_condition": cond, "Power_required": P_req, "data":prop}
         else:
             pass
     best = 100000000000000000
@@ -41,9 +41,27 @@ def find_prop(MTOW,N_prop, prop_list):
     for opt in options:
         if options[opt]["Power_required"] < best:
             best = options[opt]["Power_required"]
-            save = options[opt]
+            save_info = options[opt]
             save_name = opt
         else:
             pass
-    return (save_name,save), options
+    return (save_name,save_info), options
+
+def motor_mass(save_info):
+    RPM_max = save_info["data"].RPMmax
+    #Battery max voltage 22.2 (6S batteries)
+    kv = RPM_max / 22.2
+    P_max = save_info["data"].Power[RPM_max]
+    I_max = P_max * 22.2
+    L_motor = 4.8910 * I_max**0.1751 * P_max**0.2476
+    D_motor = 41.45 * kv **(-0.1919)*P**0.1935
+    m_motor = 0.0109 * kv**0.5122 * P_max**(-0.1902) * np.log10(L_motor) ** 2.5582 * np.log10(D_motor)**12.8502
+    return m_motor, I_max
+
+def ESC_mass(I_max):
+    return 0.8013*I_max**0.9727
+
+
+
+
         
--- propulsion/test.py
+++ propulsion/test.py
@@ -5,20 +5,23 @@ from pathlib import Path
 sys.path.insert(0, str(Path(__file__).parent.parent))
 
 from propClass import load_propeller_dict
-from Iter_function import find_prop
+from Iter_function import find_prop, motor_mass, ESC_mass
 from EPS.Iter_fuctions import power_after_efficiencies, battery_sizing
+
+# for actual iteration loop test for both 6 and 8 propellers 
 N_prop = 8
 flight_time = 10 /60 #hours
 props = load_propeller_dict("propulsion/6.0_E.csv")  #change to 6.0_E  for 8 propellers and 8.0_E for 6 propellers (yes it is confusing, sorry)
 MTOW =3.2 * 9.81 # mass * g
-P_payload = 200   #W
+P_payload = 250   #W
 P_avionics = 50   #W
 Lipo_spec_energy = 250 #Wh/kg
-best, options = find_prop(MTOW, N_prop, props)
-print(best)
+(best, best_info), options = find_prop(MTOW, N_prop, props)
+print((best, best_info))
 print(options)
-P_propellers = best[1]['Power_required']
+P_propellers = best_info['Power_required']
 P_bat = power_after_efficiencies(P_payload,P_avionics, P_propellers)
 Mass_battery = battery_sizing(P_bat, flight_time, Lipo_spec_energy)
 print(Mass_battery)
-
+m_motor, I_max = motor_mass(best_info)
+m_ESC = ESC_mass(I_max)
