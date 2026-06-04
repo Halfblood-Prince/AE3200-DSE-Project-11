f65e71ea153499a413365195e28177b0e7ac63e9
Akhil Shimna Kumar <a.shimnakumar@student.tudelft.nl>
2026-06-04 10:32:37 +0200

    chore: bending stress calculations expanded

--- bending.py
+++ bending.py
@@ -9,11 +9,22 @@ class Bending:
         self.t = t
         self.d = d
         self.inertia = pi*t*d**3/8
+        self.Mx = None
+        self.w = None
+        self.rho = None
 
-    def calculate_bending_stress(self, Mx, My, x, y):
-        return (Mx*y + My*x)/self.inertia
+    def calculate_w(self, R, t):
+        self.w = self.rho*pi*(2*R-t)*t
+
+    def calculate_maximum_longitudinal_bending_moment(self, w, L, T):
+        z_max = T/w - L
+        self.Mx = T*L - (w*L**2)/2 + w*L*z_max - T*z_max - w*(z_max**2)/2
+
+    def calculate_bending_stress(self, y):
+        return self.Mx/self.inertia*y    
+
     
-    
+
     
 
 
