e923247ee4ec69c6e0e0a690d1dfe326b21f393c
marcosMF <marcos.merinofrancos@gmail.com>
2026-06-05 09:25:00 +0200

    type ints

--- structures/shear.py
+++ structures/shear.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def shear_arm(V, t, tau_allow):
+def shear_arm(V: int, t: float, tau_allow: float = 50000) -> float:
     """
     Required outer radius Ro for a thick hollow circular rod under transverse shear.
 
