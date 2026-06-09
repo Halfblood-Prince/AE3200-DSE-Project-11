94793590a847d6b067aabe1db1b7d945b3d52ee6
JSvahn1 <julia.svahn0807@gmail.com>
2026-06-09 15:16:28 +0200

    New material properties

--- structures/bending/materials.py
+++ structures/bending/materials.py
@@ -4,9 +4,10 @@ class constants:
 
 class arm:
     # CFRP
-    name = "CFRP"
-    density = 1600                       # kg/m^3
-    youngs_modulus = 140e9               # Pa
+    name = "CFRP"                       # T700S Toray SM
+    density = 1800                       # kg/m^3
+    youngs_modulus = 230e9               # Pa
+    thickness = 7e-6                   # m
     safety_factor = 1.5
     max_tip_deflection = 0.005e-3         # m
     poisson_ratio = 0.30
@@ -19,16 +20,16 @@ class arm:
     failure_shear_stress = 50e6 # Pa
 
 class leg:
-    # Temporarily Aluminium 6061-T6
-    name = "Aluminium 6061-T6"
+    #  Aluminium 6082-T6
+    name = "Aluminium 6082-T6"
     density = 2700                       # kg/m^3
-    youngs_modulus = 69e9                # Pa
+    youngs_modulus = 70e9                # Pa
     safety_factor = 1.5
     max_tip_deflection = 0.005e-3         # m
     max_compressive_deformation = 0.01e-3 # m
     poisson_ratio = 0.33
-    yield_strength = 276e6               # Pa
-    ultimate_tensile_strength = 310e6    # Pa
+    yield_strength = 250e6               # Pa
+    ultimate_tensile_strength = 330e6    # Pa
     failure_stress = yield_strength
     failure_strain = failure_stress / youngs_modulus
     shear_modulus = youngs_modulus / (2 * (1 + poisson_ratio))
